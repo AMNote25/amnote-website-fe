7307502e0b6bc52794402c607b348dc1e257139e
AMNote25 <teamdev.amnote@gmail.com>
2025-07-30 09:05:39 +0700

    refactor: change the title of the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,10 @@ export default function RootLayout({
   return (
     <>
       <html lang="en">
+        <head>
+          <link rel="icon" type="image/svg+xml" href="/AMnote_minilogo.svg" />
+          <title>AMnote | Phần mềm kế toán nè</title>
+        </head>
         <body>{children}</body>
       </html>
     </>
